5ce31d3b2f158a206eefdf2267bb7926d5888cb7
William_S <38282661+WillSant01@users.noreply.github.com>
2024-06-23 11:18:18 +0200

    primo meeting

--- db_1.py
+++ db_1.py
@@ -9,6 +9,19 @@ r = redis.Redis(host='redis-18934.c328.europe-west3-1.gce.redns.redis-cloud.com'
                 password='4GVWbKjMnaiMtHaX56tTNKODmzblmYtq')
 print('Connesso')
 
+"""
+def crea_utente(username, password, contatti, stato):
+  # Creare un hash Redis per l'utente
+  utente_hash = {"username": username,
+                 "password": password,
+                 "contatti": contatti,
+                 "stato": stato}
+
+  # Memorizzare l'hash Redis nel database
+  r.hset("utenti", username, utente_hash)
+
+"""
+
 # Funzione di registrazione
 def registrazione():
     nome = input("Inserisci il nome utente che vuoi usare: ")
@@ -16,7 +29,10 @@ def registrazione():
     # Controllo se il nome utente esiste già
     if r.hget(f"user:{nome}", "passw") is None:
         password = input("Inserisci la password che vuoi usare: ")
-        r.hset(f"user:{nome}", mapping={"passw": password, "DnD": 0})
+        r.hset(f"user:{nome}",
+               mapping={"passw": password,
+                        "Contatti": [],
+                        "DnD": 0})
         print("Registrazione completata con successo.")
     else:
         print("Nome utente già presente. Riprova.")
@@ -28,27 +44,25 @@ def login():
 
     pass_salvate = r.hget(f"user:{nome}", "passw")
 
-    if pass_salvate is None:
-        print("Nome utente non trovato. Registrazione necessaria.")
-        return None
+    if pass_salvate == password:
+        print("Login riuscito!")
+        return nome
     else:
-        if pass_salvate == password:
-            print("Login riuscito!")
-            return nome
-        else:
-            print("Password errata.")
-            return None
+        print("Qualcosa è andato storto. Non tornare più!")
+        return None
 
 # Funzione per aggiungere contatti
 def aggiungi_contatto(nome, contatto):
-    if r.hget(f"user:{contatto}", "passw") is None:
+    if r.hget(f"user:{contatto}") is None: # Si controlla l'estistenza del contatto nel db
         print('Utente non esistente')
-    else:    
+    else:    # Dovremo controllare se il contatto è già presente nella lista
         r.rpush(f"contatti:{nome}", contatto)
-        print(f"Aggiunto {contatto} alla lista contatti di {nome}")
+        print(f"Aggiunto {contatto} alla lista contatti di {nome}") # Altrimenti lo si aggiunge.
+
 
 # Funzione per modificare la modalità Do Not Disturb
-def DND(nome):
+
+def DND(user): # User è il nome dell'utente in sessione.
     scelta1 = input("Vuoi modificare la modalità Do Not Disturb: si / no ").lower()
     if scelta1 == "si":
         val = r.hget(f"user:{nome}", "DnD")
