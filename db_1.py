9af12cfecc37f3c50c36ce83dbab54d2f0310394
Maddalena Bozzola <maddalena.bozzola@itsrizzoli.it>
2024-06-29 11:31:41 +0200

    Funzione di visualizzazione, commenti

--- db_1.py
+++ db_1.py
@@ -14,30 +14,16 @@ print('Connesso')
 def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
 
-'''
-#prova da controllare (mi piace di più)
-# Funzione di registrazione
-def registrazione(username, password):
-    if r.hexists(f"utenti:{username}", "nome"):
-        print("Nome Utente già utilizzato. Sceglierne un'altro...")
-        return False
-    password_hash = hash_password(password)
-    dati_utente = {
-        "nome": username,
-        "password": password_hash,
-        "dnd" : 0
-        }
 
-    r.hmset(f"utenti:{username}", dati_utente)
-    return True
-'''
+# Funzione che gestisce la registrazione dell'utente
 def registrazione2(username, password):
     if r.hexists(f"utenti:{username}", "nome"):
-        print("Nome Utente già utilizzato. Sceglierne un'altro...")
+        print("Nome Utente già utilizzato. Sceglierne un altro...")
         return False
     
     password_hash = hash_password(password)
     
+    # Creazione hash con nome utente e password, lista dei contatti inizializzata con un solo elemento, bitmap per il DnD
     r.hset(f"utenti:{username}","nome",username)
     r.hset(f"utenti:{username}","password",password_hash) 
     r.lpush(f"utenti:{username}:contatti", "trovati:")
@@ -45,27 +31,27 @@ def registrazione2(username, password):
 
     return True
 
-# FARE CASTING DA LISTA IN STRINGA E VICEVERSA
-# Funzione di login
+
+# Funzione per il login dell'utente
 def login(username, password):
-    if not r.hexists(f"utenti:{username}", "nome"):
+    if not r.hexists(f"utenti:{username}", "nome"): #controlla l'esistenza dell'hash associato all'utente
         print("Nome utente inesistente o password sbagliata. Riprovare...")
         return False
     
-    password_salvata = r.hget(f"utenti:{username}", "password")
+    password_salvata = r.hget(f"utenti:{username}", "password") #get della password contenuta nell'hash associato all'utente
     if password_salvata == hash_password(password):
         return True
     else:
         print("Nome utente inesistente o password sbagliata. Riprovare...")
         return False
 
-
-#ricerca utenti anche parziale
+'''
+# Ricerca utenti anche parziale
 def ricerca_utenti(nome):
     cursor = 0
     risultati = []
     while cursor == 0:
-        cursor, keys = r.scan(cursor=cursor, match = f'utenti:{nome}*')
+        cursor, keys = r.scan(cursor=cursor, match = f'utenti:{nome}*')  #scan di tutte le chiavi del db
         for key in keys:
             if r.type(key) == b'hash':
                 value = r.hget(key, nome)
@@ -74,21 +60,47 @@ def ricerca_utenti(nome):
                     if nome in value_decoded:
                         risultati.append(value_decoded)
     return risultati
+'''
 
+'''
+# Ricerca utenti 2
+def ricerca_utenti(username):
+    cursor = '0'
+    ris = []
+    while True:  # Il ciclo dovrebbe continuare finché la scansione non è completata
+        cursor, keys = r.scan(cursor=cursor, match=f'utenti:{username}*')  # Scansiona tutte le chiavi del dbl
+        for key in keys:
+            value = r.hget(key, "nome")  # Ottiene il valore del campo "nome" dell'hash
+            if value is not None:
+                value_decoded = value.decode('utf-8')
+                if username in value_decoded:
+                    ris.append(value_decoded)
+        if cursor == 0:  # Se il cursore è '0', la scansione è completata
+            break
+
+    return ris
+ '''   
 
 # Funzione per aggiungere nuovi contatti
 def aggiuntacont(risultati, username):
     utentedaagg = print(str("quale scegli?: "))
-    contatti = r.lrange("contatti", 0, -1)
+    contatti = r.lrange("contatti", 0, -1)  #restituisce una lista di redis associata alla chiave contatti
     for el in risultati:
         if utentedaagg not in contatti and el == utentedaagg:
-            r.rpush(f"utenti:{username}:contatti", utentedaagg)
+            r.rpush(f"utenti:{username}:contatti", utentedaagg) #comando che aggiunge l'elemento nella lista
             print("i tuoi contatti sono: \n", contatti)
         else:
             print("errore")
             break
 
-# Funzione primo menù
+# Funzione per la visualizzazione della lista dei contetti
+def vis_contatti(username):
+    contatti = r.lrange(f"utenti:{username}:contatti", 0, -1)  #recupera i contatti dell'utente
+    if contatti:
+        for contatto in contatti:
+            print(contatto)
+
+# Prima parte del menù, gestisce registrazione, login e DnD
 def main(loggato = False):
     while True:
         scelta = input("Vuoi (r)egistrati oppure effettuare il (l)ogin? (q) per uscire ").lower()
@@ -111,13 +123,13 @@ def main(loggato = False):
             usernameloggato = username
             main2(usernameloggato, loggato)
 
-            valdnd = r.getbit(f"utenti:{usernameloggato}:dnd",0)
+            valdnd = r.getbit(f"utenti:{usernameloggato}:dnd",0)  # comando che estrae il bitmap DnD associato all'utente loggato
             if valdnd == 1:
                 print("Do Not Disturb attivo")
             else:
                 print("Do Not Disturb disattivo")
 
-#secondo menù
+# Seconda parte del menù, gestisce tutte le altre azioni
 def main2(usernameloggato, loggato):
     while True:
         scelta = input("""Benvenuto! Vuoi:
@@ -139,15 +151,16 @@ def main2(usernameloggato, loggato):
                 print("risultati", risultati)
             
             case "v":
-                pass
-
+                valori = vis_contatti(usernameloggato)
+                print(valori)
+                
             case "d":
-                valdnd = r.getbit(f"utenti:{usernameloggato}:dnd",0)
+                valdnd = r.getbit(f"utenti:{usernameloggato}:dnd",0) # estrazione del bitmap DnD associato all'utente loggato
                 if valdnd == 0:
-                    r.setbit(f"utenti:{usernameloggato}:dnd",0, 1)
+                    r.setbit(f"utenti:{usernameloggato}:dnd",0, 1) # modifica del valore in "attivo"
                     print("Do Not Disturb attivato")
                 else:
-                    r.setbit(f"utenti:{usernameloggato}:dnd",0, 0)
+                    r.setbit(f"utenti:{usernameloggato}:dnd",0, 0) # modifica del valore in "disattivato"
                     print("Do Not Disturb disattivato")
 
             
